2f7ad0d4785a07c1a9a4826fda4a49582abce0cc
Anurag12133 <anurajput62616@gmail.com>
2024-06-04 02:47:59 +0530

    Module addede

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Providers>{children}</Providers>
+        <Providers> {children}</Providers>
       </body>
     </html>
   );
